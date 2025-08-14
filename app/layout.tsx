fda7d28bd9ccb669b52ba13f021ee05a163241f0
qilimk <qili.mk@gmail.com>
2025-08-14 00:16:06 -0500

    minor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import Script from "next/script";
 import NavBar from "@/components/NavBar";
